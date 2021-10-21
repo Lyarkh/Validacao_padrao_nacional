2e2d3c5cdb189fbf08359f4d934136950fa2ccf5
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-10-21 09:58:13 -0300

    Dando inicio a classe Cpf e minitestes com a classe

--- Cpf.py
+++ Cpf.py
@@ -1,6 +1,16 @@
 
 class Cpf:
     def __init__(self, documento):
-        pass
+        documento = str(documento)
+        if self.cpf_eh_valido(documento):
+            self.cpf = documento
+        else:
+            raise ValueError("CPF inválido!!")
+
+    def cpf_eh_valido(self, documento):
+        if len(documento) == 11:
+            return True
+        else:
+            return False
 
         
---
+++ main.py
@@ -0,0 +1,5 @@
+from Cpf import Cpf
+
+cpf = 50456925125
+
+criando_objeto = Cpf(cpf)
