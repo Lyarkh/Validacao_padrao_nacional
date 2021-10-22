8525c324010ab066b6ce010c667c6e0789183366
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-10-22 15:39:00 -0300

    Atualizando classe

--- Cpf.py
+++ Cpf.py
@@ -1,6 +1,36 @@
-from validate_docbr import CPF
+from validate_docbr import CPF, CNPJ
+from abc import ABCMeta, abstractmethod
 
-class Cpf:
+class Documento(metaclass = ABCMeta):
+
+    def __str__(self):
+        return self.formata()
+
+    @abstractmethod
+    def formata(self):
+        pass
+
+class Cnpj(Documento):
+    def __init__(self, documento):
+        documento = str(documento)
+        if self.cnpj_eh_valido(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError("CNPJ inválido!!")  
+
+    def cnpj_eh_valido(self, cnpj):
+        if len(cnpj)==14:
+            validate_cnpj = CNPJ()
+            return validate_cnpj.validate(cnpj)
+
+        else:
+            raise ValueError("Quantidade de dígitos inválida!")
+
+    def  formata(self):
+        return self.cnpj
+        
+class Cpf(Documento):
+
     def __init__(self, documento):
         documento = str(documento)
         if self.cpf_eh_valido(documento):
@@ -8,16 +38,13 @@ class Cpf:
         else:
             raise ValueError("CPF inválido!!")
 
-    def __str__(self):
-        return self.formata_cpf()
-
     def cpf_eh_valido(self, cpf):
         if len(cpf) == 11:
             validando = CPF()
             return validando.validate(cpf)
         else:
-            return ValueError ("Quantidade de dígitos inválido")
+            raise ValueError("Quantidade de dígitos inválido!!")
 
-    def formata_cpf(self):
+    def formata(self):
         mascara = CPF()
         return mascara.mask(self.cpf)        
