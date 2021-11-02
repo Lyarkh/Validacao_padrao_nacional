5c132913d192f44a33808f97006e8ee5b28c17d8
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-11-02 18:13:48 -0300

    Melhorando a classe telefone e testando ela

--- Telefone.py
+++ Telefone.py
@@ -8,11 +8,25 @@ class TelefoneBR:
         else:
             raise ValueError("Numero Incorreto!")    
     
+    def __str__(self):
+        return self.formata()
+
+    def padrao_numero(self):
+        return "([0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+
     def valida_telefone(self, telefone):
-        padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+        padrao = self.padrao_numero()
         resposta = re.findall(padrao, telefone)
 
         if resposta:
             return True
         else:
             return False
+
+    def formata(self):
+        padrao =self.padrao_numero()
+        res = re.search(padrao, self.telefone)
+        numero_formatado = (f"+{res.group(1)} ({res.group(2)}) {res.group(3)}-{res.group(4)}")
+        return numero_formatado
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Testes das classes do arquivo
 from Documentos import Documento
+from Telefone import TelefoneBR
 
 """ cpf = Documento.cria_documento("05456925125")
 print(cpf)
@@ -8,8 +9,14 @@ cnpj =Documento.cria_documento("35379838000112")
 print(cnpj) 
  """
 
-teste = input("Digite seu documento com apenas números: ")
+""" teste = input("Digite seu documento com apenas números: ")
 
 teste = Documento.cria_documento(teste.strip())
 
-print(f"Seu documento formatado é: {teste}")
+print(f"Seu documento formatado é: {teste}") """
+
+numero = "5561986067711"
+
+numero = TelefoneBR(numero)
+
+print(numero)
